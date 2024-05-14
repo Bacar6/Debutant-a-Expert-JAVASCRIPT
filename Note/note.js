74728f515dc9379c396f3da318119da984d9dc43
gala <andhoumani13003.g@gmail.com>
2024-05-14 14:36:03 +0200

    function sur supprimer

--- Note/note.js
+++ Note/note.js
@@ -15,7 +15,19 @@ generateurNote = (event) => {
 
   champResult.textContent = textInput;
 
-  console.log(champResult);
+  // Fonction pour créer un bouton avec du texte et une action
+  const creationButton = (text, action) => {
+    const button = document.createElement("button");
+
+    button.textContent = text;
+    button.classList.add("button");
+    button.addEventListener("click", action);
+    return button;
+  };
+  // Création des boutons avec leur texte et leur action associée
+  const buttonSupprimer = creationButton("supprimer", () => {
+    champResult.remove();
+  });
 
   champInput.value = "";
 };
