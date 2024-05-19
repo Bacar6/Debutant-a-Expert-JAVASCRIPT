73fc8f756fe4ca096a62d34732e430a2e4e47772
gala <andhoumani13003.g@gmail.com>
2024-05-19 09:32:23 +0200

    2eme Algo

---
+++ ALGORITHME Affaire/Algo-2/algo2.js
@@ -0,0 +1,19 @@
+// EXERCICE 2
+
+// Crée une fonction qui prend en paramètre un tableau et qui retourne le premier élément du tableau.
+// Exemple : [1, 2, 3] => 1
+// Pour rappel pour accéder à un élément d'un tableau on utilise la méthode [laplaceDuNombreDeL'élément] :
+// const monTableau = [1, 2, 3]
+// console.log(monTableau[0]) // 1
+
+// CODE ICI -----------------------------------------------------------------------//
+
+const premierElement = (arr) => {
+  return arr[0];
+};
+
+const monTableau = [1, 2, 3];
+
+console.log(premierElement(monTableau));
+
+//----------------------------------------------------------------------------------------------//
