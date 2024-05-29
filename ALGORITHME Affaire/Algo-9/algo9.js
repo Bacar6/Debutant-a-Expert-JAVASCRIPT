65f572c53128abd7c45bb858fd92a6f5e96eaabf
gala <andhoumani13003.g@gmail.com>
2024-05-29 22:10:50 +0200

    Ajout Algo 9

---
+++ ALGORITHME Affaire/Algo-9/algo9.js
@@ -0,0 +1,11 @@
+// EXERCICE 9
+
+// Crée une fonction qui prend en paramètre un tableau et qui doit retourner le tableau trié par ordre croissant.
+// Exemple : [1, 3, 2] => [1, 2, 3]
+// Pour cette exercice on va utiliser la méthode sort() qui permet de trier un tableau :
+// const monTableau = [1, 3, 2]
+// console.log(monTableau.sort()) // [1, 2, 3]
+
+// CODE ICI ----------------------------------------------------------//
+
+//----------------------------------------------------------------------------------------------//
